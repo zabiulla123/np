af5449724ca4ac104bd7a9b4d78d8952e8c02dfc
zabiulla123 <129505365+zabiulla123@users.noreply.github.com>
2023-08-17 12:22:10 +0530

    Update np.py

--- np.py
+++ np.py
@@ -8,3 +8,14 @@ print("maximum between array = ",arr.max(axis=1))
 print("cumulatite sum = ",arr.cumsum(axis=1))
 print("average of the array = ",np.average(arr))
 
+
+import pandas as pd
+data = pd.read_csv("C:/Users/SPTINT-24/Desktop/data set mn/elon_musk_tweets.csv")
+print (data)
+print(data.head(5))
+print(data.tail(10))
+print (data.info())
+print(data.dtypes)
+print(data.count())
+print(data["retweets"])
+
